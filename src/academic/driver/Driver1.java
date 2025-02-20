f6b24aa0d236541d1553e395b40ab2e7ae65e2b8
ChintyaRajagukguk <143678980+ChintyaRajagukguk@users.noreply.github.com>
2025-02-20 09:46:42 +0700

    Update Driver1.java

--- src/academic/driver/Driver1.java
+++ src/academic/driver/Driver1.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Driver1 {
     public static void main(String[] _args) {
         Scanner sc = new Scanner(System.in);
-        LinkedList<Course> courses = new LinkedList<>(); // Menggunakan LinkedList agar addFirst() bisa dipakai
+        LinkedList<Course> courses = new LinkedList<>();
         LinkedHashSet<Student> students = new LinkedHashSet<>();
         LinkedHashSet<Enrollment> enrollments = new LinkedHashSet<>();
 
